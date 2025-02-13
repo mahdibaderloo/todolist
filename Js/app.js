4c8930ed6fe4d18dcc7422f2abf11ddcbf68c522
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-02-13 19:35:44 +0330

    add tasks to dom

--- Js/app.js
+++ Js/app.js
@@ -53,30 +53,46 @@ submitTaskButton.addEventListener('click', () => {
         date : dateTimeInput.value,
         description : descriptionInput.value
     }
-    pushToTasks(data)
+
+    if (getLocalStorageData()) {
+        let localTasks = getLocalStorageData()
+        tasks = []
+        localTasks.forEach (task => {
+            task.push(task)
+        })
+    } else {
+        tasks.push(data)
+        addDataToLocalStorage(tasks)
+        clearDom()
+        addTasksToDom(tasks)
+    }
 })
 
-function createTask (tasks) {
-    tasksWrapper.insertAdjacentHTML('beforeend', `
-        <li id="task-box" class="w-full flex items-center rounded-xl shadow-[2px_2px_10px_1px_rgba(0,0,0,0.15)] pl-4 pr-4 pt-1 pb-1">
-            <div id="done" class="w-11 h-11 flex justify-center items-center bg-teal-600 rounded-xl">
-                <img src="Images/Svg/tick.svg" alt="" id="tick-icon" class="">
-            </div>
-            <div id="task-information" class="flex flex-col gap-0.5 ml-4">
-                <p id="task-title" class="text-2xl font-bold">Title</p>
-                <div id="time-box">
-                    <p id="task-time" class="text-[14px] opacity-60">9:00 - 23:00</p>
-                    <p id="task-date" class="text-[14px] opacity-60">16 April, Friday</p>
-                </div>
-            </div>
-            <img src="Images/Svg/arrow-right.svg" alt="" id="task-setting-icon" class="ml-auto mr-3 w-3">
-        </li>`)
-}
+
+// Add Tasks To DOM //
+
+// function addTasksToDom (tasks) {
+//     tasks.forEach (task => {
+//         tasksWrapper.insertAdjacentHTML('beforeend', `
+//             <li id="task-box" class="w-full flex items-center rounded-xl shadow-[2px_2px_10px_1px_rgba(0,0,0,0.15)] pl-4 pr-4 pt-1 pb-1">
+//                 <div id="done" class="w-11 h-11 flex justify-center items-center bg-teal-600 rounded-xl">
+//                     <img src="Images/Svg/tick.svg" alt="" id="tick-icon" class="hidden">
+//                 </div>
+//                 <div id="task-information" class="flex flex-col gap-0.5 ml-4">
+//                     <p id="task-title" class="text-2xl font-bold">${task.title}</p>
+//                     <div id="time-box">
+//                         <p id="task-time" class="text-[14px] opacity-60">9:00 - 23:00</p>
+//                         <p id="task-date" class="text-[14px] opacity-60">16 April, Friday</p>
+//                     </div>
+//                 </div>
+//                 <img src="Images/Svg/arrow-right.svg" alt="" id="task-setting-icon" class="ml-auto mr-3 w-3">
+//             </li>`)
+//     })
+// }
 
 
-// Tasks Array //
+// Clear DOM Tasks //
 
-function pushToTasks (data) {
-    tasks.push(data)
-    addDataToLocalStorage(tasks)
+function clearDom () {
+    tasksWrapper.innerHTML = ''
 }
